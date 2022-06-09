58c716eb2fc595956ce3baa0ffe72763161d08cd
Adeniltonarcanjo <Adeniltonmorais@gmail.com>
2022-06-08 21:44:29 -0300

    Projection of author data with DTO

--- src/main/java/com/github/adeniltonarcanjo/workshopmongo/repository/UserRepository.java
+++ src/main/java/com/github/adeniltonarcanjo/workshopmongo/repository/UserRepository.java
@@ -3,9 +3,10 @@ package com.github.adeniltonarcanjo.workshopmongo.repository;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import com.github.adeniltonarcanjo.workshopmongo.domain.Post;
 import com.github.adeniltonarcanjo.workshopmongo.domain.User;
 
 @Repository
-public interface UserRepository extends MongoRepository<User, String> {
+public interface UserRepository extends MongoRepository<Post, String> {
 
 }
